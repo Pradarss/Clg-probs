a539b4b396a71706cf1141cd3fd1a153077a26c9
Crucifer <verma0025adarsh@gmail.com>
2022-12-12 23:10:19 +0530

    ...

--- OOPs/test1.cpp
+++ OOPs/test1.cpp
@@ -15,16 +15,17 @@ public:
     }
     void NewSerial(int serial_code, char title[])
     {
-        serialCode = serial_code;
-        for (int i = 0; (i < 20 && title[i] != '\0'); i++)
+        serialCode = serial_code; //1
+        for (int i = 0; (i < 20 && title[i] != '\0'); i++)    //2(n+1)
+         //       1             n+1                     n                                             
         {
-            this->title[i] = title[i];
+            this->title[i] = title[i]; //n
         }
     }
     void otherEntries(float duration, int noOfEpisodes)
     {
-        duration = duration;
-        noOfEpisodes = noOfEpisodes;
+        duration = duration; //1
+        noOfEpisodes = noOfEpisodes; //1
     }
     void dispData()
     {
@@ -36,15 +37,17 @@ public:
 };
 int main()
 {
-    char serial1[] = "Breaking Bad";
-    char serial2[] = "Better Call Saul";
+    char serial1[] = "Breaking Bad"; //1
+    char serial2[] = "Better Call Saul";  //1
     Serial S1;
-    S1.NewSerial(101, serial1);
+    S1.NewSerial(101, serial1); //2(n^2)+3
     S1.dispData();
     cout << endl;
     Serial S2;
-    S2.NewSerial(102, serial2);
-    S2.otherEntries(40, 60);
+    S2.NewSerial(102, serial2); //2(n^2)+3
+    S2.otherEntries(40, 60); //1
     S2.dispData();
     return 0;
 }
+
+//f(n)=4(n^2)+9 => o(n^2)
