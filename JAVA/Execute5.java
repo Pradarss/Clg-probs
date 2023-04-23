9f9ca9bcfdb43281a50e97f08c566cc205c400bb
Pradarss <verma0025adarsh@gmail.com>
2023-04-23 16:45:50 +0530

    ..

--- JAVA/Execute5.java
+++ JAVA/Execute5.java
@@ -1,24 +1,23 @@
 class Fibonacci_with_recursion {
     int n1 = 0, n2 = 1, n3 = 0;
-
+      
     void printFibonacci(int count) {
-        if (count > 0) {
-            // System.out.print(0);
+      if (count > 0) {
             n3 = n1 + n2;
             n1 = n2;
             n2 = n3;
-            System.out.print(" " + n3);
+              System.out.print("  " + n3);
             printFibonacci(count - 1);
         }
     }
 }
 
 class Fibonacci_without_recursion {
-    int n1 = 0, n2 = 1, n3 = 0;
+    int n1 = 0, n2 = 1, n3;
 
     void fibonaaci2(int count) {
-        System.out.print(0);
-        for (int i = 2; i < count; ++i)// loop starts from 2 because 0 and 1 are already printed
+        System.out.print(n1+ " " +n2+" ");
+        for (int i = 2; i < count; ++i)
         {
             n3 = n1 + n2;
             System.out.print(" " + n3);
@@ -31,13 +30,17 @@ class Fibonacci_without_recursion {
 
 class Execute5 {
     public static void main(String[] args) {
-        System.out.println("Adarsh");                      
+        System.out.println("Adarsh");                        
+        int count=10;
         Fibonacci_with_recursion f = new Fibonacci_with_recursion();
         System.out.println("Fibonacci with recursion : ");
-        f.printFibonacci(7);
+	System.out.print(f.n1 + " " + f.n2);
+        f.printFibonacci(count-2);
         Fibonacci_without_recursion f2 = new Fibonacci_without_recursion();
+        
         System.out.println("\n");
         System.out.println("Fibonacci without recursion : ");
-        f2.fibonaaci2(7);
+        f2.fibonaaci2(count);
     }
 }
+
