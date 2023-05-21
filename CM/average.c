8f0bf732073c78a334e4e02e29b5c486c6f1f991
Pradarss <verma0025adarsh@gmail.com>
2023-05-21 21:21:24 +0530

    ..

--- CM/average.c
+++ CM/average.c
@@ -8,7 +8,7 @@ int main(){
     
     avg = sum / 10.0;
     
-    printf("Sum of 10 numbers:\n 1+2+3+4+5+6+7+8+9+10 = %d\n\nAverage of 10 numbers:\n (1+2+3+4+5+6+7+8+9+10)/10 = %.2f", sum, avg);
+    printf("Sum of 10 numbers:\n 1+2+3+4+5+6+7+8+9+10 = %d\n\nAverage of 10 numbers:\n (1+2+3+4+5+6+7+8+9+10)/10 = %.2f\n", sum, avg);
 
     return 0;
 }
