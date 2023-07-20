46d5b24e0537d65efb37a4eeca869d93375c7178
Pradarss <verma0025adarsh@gmail.com>
2023-07-20 20:21:11 +0530

    ..

---
+++ Adarsh/trie.java
@@ -0,0 +1,33 @@
+// import java.io.File;
+// import java.io.FileWriter;
+// import java.io.IOException;
+
+// class trie{
+//     public static void main(String[] args){
+//         try{
+//             FileWriter fw = new FileWriter("trie.txt");
+//             fw.close();
+//         }
+//         catch(IOException e){
+//             System.out.println("error");
+//         }
+//     }
+// }
+
+import java.awt.*;
+import java.time.format.TextStyle;
+import java.applet.*;
+
+/*<applet code="trie" width=300 height=400> </applet>*/
+
+class trie extends Applet{
+    String data;
+    TextField t1,t2;
+    TextArea res;
+
+    class init{
+        setBackground(color.blue);
+        setForeground(color.white);
+
+    }
+}
